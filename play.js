d9f9e3dade5b8529d2b1db63acf77915df7d666b
muezwink <muezwink@gmail.com>
2024-01-25 17:15:44 +0900

    스프레드(Spread) 및 레스트(Rest) 연산자의 이해

--- play.js
+++ play.js
@@ -6,11 +6,20 @@ const person = {
   }
 };
 
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
 const hobbies = ['Sports', 'Cooking'];
 // for (let hobby of hobbies) {
 //     console.log(hobby);
 // }
 // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
 // console.log(hobbies);
-hobbies.push('Programming');
-console.log(hobbies);
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const toArray = (...args) => {
+  return args;
+};
+
+console.log(toArray(1, 2, 3, 4));
