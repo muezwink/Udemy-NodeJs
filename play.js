d8b7cf26d82eae20ee50efe9156edb884c14f1ca
muezwink <muezwink@gmail.com>
2024-01-25 17:01:32 +0900

    배역, 객체 및 참조 유형

--- play.js
+++ play.js
@@ -10,5 +10,7 @@ const hobbies = ['Sports', 'Cooking'];
 // for (let hobby of hobbies) {
 //     console.log(hobby);
 // }
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+hobbies.push('Programming');
 console.log(hobbies);
