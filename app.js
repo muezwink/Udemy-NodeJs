1e38dd643abbbfec0672d61807417a47f19904d0
muezwink <muezwink@gmail.com>
2024-02-12 22:45:13 +0900

    65. POST 요청으로 미들웨어 실행 제한

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extend: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body)
-  res.redirect('/')
-})
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
