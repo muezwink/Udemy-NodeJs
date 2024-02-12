ba81007049290b321680472793988354ab21b9c6
muezwink <muezwink@gmail.com>
2024-02-12 22:41:38 +0900

    64. 수신 요청 분석

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extend: false}))
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
 app.use('/', (req, res, next) => {
     console.log('This always runs!');
     next();
